7a3eb50998109800d3957e18e5e84f5307dbac8b
TA1059 <muchsen.ft21@mail.umy.ac.id>
2022-11-14 17:06:51 +0700

    add doublelinkedlist and list empty

--- double_linked_list/Program.cs
+++ double_linked_list/Program.cs
@@ -1,1 +1,63 @@
-﻿
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace double_linked_list
+{
+    class Node
+    {
+        public int noMhs;
+        public string name;
+        //
+        public Node next;
+        //
+        public Node prev;
+
+        static void Main(string[] args)
+        {
+
+        }
+    }
+
+    class DoubleLinkedList
+    {
+        Node START;
+
+        //
+
+        public DoubleLinkedList()
+        {
+            START = null;
+        }
+
+        public void addNode()
+        {
+            int nim;
+            string nm;
+            Console.WriteLine("\nEnter the roll number of the student");
+            nim = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("\nEnter the name of the student: ");
+            nm = Console.ReadLine();
+            Node newNode = new Node();
+            newNode.noMhs = nim;
+            newNode.name = nm;
+
+            //check if the list empty
+            if (START == null || nim <= START.noMhs)
+            {
+                if((START != null) && (nim == START.noMhs))
+                {
+                    Console.WriteLine("\nDuplicade number not allowed");
+                    return;
+                }
+                newNode.next = START;
+                if (START != null)
+                    START.prev = newNode;
+                newNode.prev = null;
+                return;
+            }
+        }
+    }
+}
